abe312b3f18fe0e125a3a3f5652bca149de5a1db
guswjdl1234 <guswjddudwo@gmail.com>
2023-02-20 20:42:52 +0900

    틀정하기

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import LecipeAPI from './LecipeAPI';
 import LecipeAPI2 from './temp/LecipeAPI2';
+import subpage from './pages/subpage/subpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/subpage/subpage.jsx
+++ src/pages/subpage/subpage.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import styled from "styled-components";
+
+
+const nav = styled.nav`
+    background: #000;
+`
+
 
 function subpage(props) {
     return (
         <div>
-            
+           <nav>NAV영역</nav>
+           <div>
+            <h1>레시피종류</h1>
+            <img>분류1</img>
+            <img>분류2</img>
+            <img>분류3</img>
+            </div> 
+            <div>
+                <h1>오늘의 요리 결과물들</h1>
+                <Table>
+          <tr>
+            <th>No</th>
+            <th>상품명</th>
+            <th>수량</th>
+            <th>가격</th>
+            <th>삭제</th>
+          </tr>
+      </Table>
+            </div>
         </div>
     );
 }
