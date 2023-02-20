0e21833043e71c86d7b1eedab4efa51b1f85dcf0
leehoeeun <dyssid0722@gmail.com>
2023-02-20 18:17:47 +0900

    recipe API 연결(미완성)

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import LecipeAPI from './LecipeAPI';
-import LecipeAPI2 from './temp/LecipeAPI2';
+// import LecipeAPI from './LecipeAPI';
+import LecipeAPI2 from '../src/temp/LecipeAPI2';
+import RecipeDetail from './pages/recipe/RecipeDetail';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    // <App />
     // <LecipeAPI2 />
+    <RecipeDetail />
 );
