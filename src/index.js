eacda290db2ea45e8587766ac1da16a9b8ecc173
guswjddudwo <guswjddudwo@gmail.com>
2023-02-21 17:42:37 +0900

    css추가,태그수정

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import LecipeAPI from './LecipeAPI';
-import LecipeAPI2 from './temp/LecipeAPI2';
-import subpage from './pages/subpage/subpage';
+// import LecipeAPI from './LecipeAPI';
+// import LecipeAPI2 from './temp/LecipeAPI2';
+import Subpage from './pages/subpage/Subpage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    <>
     <App />
-    // <LecipeAPI2 />
+    <Subpage />
+    </>
 );
--- src/pages/subpage/subpage.jsx
+++ src/pages/subpage/subpage.jsx
@@ -2,40 +2,40 @@ import React from 'react';
 import styled from "styled-components";
 
 
-const nav = styled.nav`
+const Nav = styled.nav`
     background: #000;
-`
+`;
+// w 1200
+const Ennen = styled.div`
+`;
+
+const box = styled.div`
+    width: 1200px;
+    display: flex;
+    
+`;
 
 
-function subpage(props) {
+function Subpage(props) {
     return (
         <div>
            <nav>NAV영역</nav>
            <div>
             <h1>레시피종류</h1>
-            <img>분류1</img>
-            <img>분류2</img>
-            <img>분류3</img>
+            <img src='https://via.placeholder.com/285x285' alt='#'/>
+            <img src='https://via.placeholder.com/285x285' alt='#'/>
+            <img src='https://via.placeholder.com/285x285' alt='#'/>
+        
             </div> 
             <div>
                 <h1>오늘의 요리 결과물들</h1>
-                <Table>
-          <tr>
-            <th>카드1</th>
-            <th>카드2</th>
-            <th>카드3</th>
-            <th>카드4</th>
-          </tr>
-          <tr>
-            <th>카드1</th>
-            <th>카드2</th>
-            <th>카드3</th>
-            <th>카드4</th>
-          </tr>
-      </Table>
+                    <img src='https://via.placeholder.com/285x285' alt='#'/>
+                    <img src='https://via.placeholder.com/285x285' alt='#'/>
+                    <img src='https://via.placeholder.com/285x285' alt='#'/>
+                    <img src='https://via.placeholder.com/285x285' alt='#'/>
             </div>
         </div>
     );
 }
 
-export default subpage;
+export default Subpage;
