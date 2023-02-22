5335a6b57b33007632fa4700f55527bd5590e96a
sangmin Lee <smha77@naver.com>
2023-02-22 17:48:17 +0900

    아이콘 변경안됌

--- src/pages/main/login.jsx
+++ src/pages/main/login.jsx
@@ -4,20 +4,19 @@ import styles from './Login.module.css'
 import { CgProfile } from "react-icons/cg";
 import styled from "styled-components";
 
-const BtnStyled = styled.button`
-  background-image: url();
-`
+
 function Login(props) {
   return (
     <Nav className={styles.wrap}>
-    <Nav.Link className={styles.lo} href="#deets">로그인</Nav.Link>
-    <Nav.Link className={styles.ic} href="#deets"> <h4><button></button></h4></Nav.Link>
-    <Nav.Link className={styles.ic} href="#deets"> <h4><CgProfile/></h4></Nav.Link>
-    
-    <Nav.Link className={styles.lo} eventKey={2} href="#memes">
-      회원가입
-    </Nav.Link>
-  </Nav>
+      <Nav.Link className={styles.lo} href="#deets">로그인</Nav.Link>
+      <Nav.Link className={styles.ic} href="#deets">
+        <button type='button' className='btn-keeper'></button>
+      </Nav.Link>
+      <Nav.Link className={styles.ic} href="#deets"> <h4><CgProfile /></h4></Nav.Link>
+      <Nav.Link className={styles.lo} eventKey={2} href="#memes">
+        회원가입
+      </Nav.Link>
+    </Nav>
   );
 }
 
