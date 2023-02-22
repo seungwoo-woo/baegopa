70e7d5371fd2df167a2292047d7132f139478f0c
sangmin Lee <smha77@naver.com>
2023-02-22 17:37:48 +0900

    스와이퍼 해더 오늘의요리 수정했습니다.

---
+++ src/pages/main/view/LastView.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
+
+function LastView(props) {
+  return (
+    <ListGroupItem>
+      <ListGroup.Item>Cras justo odio</ListGroup.Item>
+      <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+      <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+      <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+      <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+    </ListGroupItem>
+  );
+}
+
+export default LastView;
