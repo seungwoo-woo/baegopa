93bb597e8f458141f045f7b723cda7e301671cf7
sangmin Lee <smha77@naver.com>
2023-02-22 17:41:28 +0900

    123

--- src/pages/main/login.jsx
+++ src/pages/main/login.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import styles from './Login.module.css'
-import { BsBookmarkPlus } from "react-icons/bs";
 import { CgProfile } from "react-icons/cg";
+import styled from "styled-components";
+
+const BtnStyled = styled.button`
+  background-image: url();
+`
 function Login(props) {
   return (
     <Nav className={styles.wrap}>
     <Nav.Link className={styles.lo} href="#deets">로그인</Nav.Link>
-    <Nav.Link className={styles.ic} href="#deets"> <h4><BsBookmarkPlus/></h4></Nav.Link>
+    <Nav.Link className={styles.ic} href="#deets"> <h4><button></button></h4></Nav.Link>
     <Nav.Link className={styles.ic} href="#deets"> <h4><CgProfile/></h4></Nav.Link>
     
     <Nav.Link className={styles.lo} eventKey={2} href="#memes">
