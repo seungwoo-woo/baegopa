663f822aaccf13e518807bfc647da6b5e61eaf8b
leehoeeun <dyssid0722@gmail.com>
2023-02-27 17:48:52 +0900

    [테스트] 각 페이지를 App.js에서 확인

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css' //부트스트랩 적용 
 import 'react-toastify/dist/ReactToastify.min.css'; // ReactToastify CSS 추가
 import Main from './pages/main/Main';
+import Subpage from './pages/subpage/Subpage';
+import Footer from './pages/subpage/Footer';
 
 
 
@@ -11,7 +13,9 @@ function App() {
   return (
     <div className="App">
       <div>
-        <Main />
+        {/* <Main /> */}
+        <Subpage />
+        <Footer />
       </div>
     </div>
   );
