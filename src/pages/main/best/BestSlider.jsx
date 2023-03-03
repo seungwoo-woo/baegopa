eb6831d805c6c8c3de917a2f7516cba5c17911aa
sangmin131 <smha77@naver.com>
2023-03-03 20:01:37 +0900

    기본적인 나머지 css정리완료

--- src/pages/main/best/BestSlider.jsx
+++ src/pages/main/best/BestSlider.jsx
@@ -3,31 +3,8 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './BestSlider.module.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
+import 'swiper/css/pagination';
 import BestCard from './BestCard';
-// <<<<<<< HEAD
-
-// TODO: 상민님 확인 필요
-// const StyledSwiper = styled(Swiper)`
-//   width: 1250px;
-//   height: 400px;
-//   display: flex;
-//   margin-top: -10px;
-//   flex-wrap: nowrap;
-//   .swiper-button-next{
-//   background-image: url('../img/mingcute_bowl-line.png');
-//   }
-//   .swiper-button-next, .swiper-button-prev {
-//     width: 35px;
-//     height: 50px;
-//     color: #EEEEEE; /* 원하는 색깔로 변경 */
-//     background-color: #bdbdbd5c;
-//   }
-//   .swiper-container {
-//   height: 100%;
-
-// }
-// `;
 
 
 import Best from './Best';
@@ -46,7 +23,7 @@ function BestSlider(props) {
         spaceBetween={40}
         slidesPerView={4}
         navigation={{ prevEl: `.${styles['prev-button']}`, nextEl: `.${styles['next-button']}` }}
-        pagination={{ clickable: true, }}
+        pagination={{ clickable: true }}
       >
         <SwiperSlide className={styles.swiperi}><BestCard /></SwiperSlide>
         <SwiperSlide className={styles.swiperi}><BestCard /></SwiperSlide>
