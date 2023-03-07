d9303578e119dce68dc459a43b201153633ca849
seungwoo-woo <seungwoo.woo@gmail.com>
2023-03-07 18:39:05 +0900

    3월7일 학원 (서치 완성)

--- src/App.js
+++ src/App.js
@@ -12,15 +12,13 @@ import SearchPage from './pages/search/SearchPage';
 import BestSlider from './pages/main/best/BestSlider';
 import NomatchPage from './pages/main/NomatchPage';
 import Main from './pages/main/Main';
-import styles from './App.css'
+import styles from './App.css';
 import GetRecipeDB from './pages/addrecipe/GetRecipeDB';
 import CardList from './components/CardList';
 import Subpage from './pages/subpage/Subpage';
 import Signin from './pages/login/Signin';
 import Signup from './pages/login/Signup';
 import MovePage from './pages/login/MovePage';
-import Subpage from './pages/subpage/Subpage';
-import CardList from './components/CardList';
 import Mypage from './pages/mypage/Mypage';
 
 
@@ -31,16 +29,16 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/movepage" element={<MovePage />} />
-          {/* <Route path="/" element={<Main />} /> */}
-          {/* <Route path="/" element={<Subpage />} /> */}
-          {/* <Route path="/today" element={<Today />} />
-          <Route path="/best" element={<BestSlider />} /> */}
-          {/* <Route path="/search" element={<SearchPage />} /> */}
-          {/* <Route path="/recipe" element={<RecipeDetail />} /> */}
-          {/* <Route path="*" element={<NomatchPage />} /> */}
+          <Route path="/" element={<Main />} /> */}
+          <Route path="/" element={<Subpage />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/best" element={<BestSlider />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/recipe" element={<RecipeDetail />} />
+          <Route path="*" element={<NomatchPage />} />
         </Routes>
         <Footer />
       </BrowserRouter>
